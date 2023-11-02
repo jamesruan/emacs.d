2cdd168e70364e09f2b66a46ee177faee44b6429
James Ruan <ruanbeihong@gmail.com>
2023-11-02 16:46:44 +0800

    update

--- elpa/bazel-20221105.2049/bazel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from bazel.el  -*- no-byte-compile: t -*-
-(define-package "bazel" "20221105.2049" "Bazel support for Emacs" '((emacs "27.1")) :commit "68d58547dd1df92adc91715f97c3dcd125b00810" :keywords '("build tools" "languages") :url "https://github.com/bazelbuild/emacs-bazel-mode")
